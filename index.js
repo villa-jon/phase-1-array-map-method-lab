7c588298144e7d0ed36be303dc5282c3c6f45a19
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-05-06 08:15:19 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const tutorials = [
-  'what does the this keyword mean?',
+  'What does the this keyword mean?',
   'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
+  'Implementing Blockchain Web API',
   'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
   'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'What is the difference between == and ===?',
+  'What is the difference between event capturing and bubbling?',
+  'What is JSONP?'
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(x => 
+    x.split(' ').map(x => 
+      x.charAt(0).toUpperCase() +
+     x.slice(1)).join(' '))
+    
 }
